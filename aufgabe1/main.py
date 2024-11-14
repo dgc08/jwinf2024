26f9f89b7127ab9d6e54fa5ed1fb27e17eccba89
Valentin Singer <SingerV-gar@gar-vs.de>
2024-11-14 21:52:06 +0100

    aufgabe 1 code

--- aufgabe1/main.py
+++ aufgabe1/main.py
@@ -1,14 +1,46 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 from sys import argv
+from math import ceil
+
+def q(x,y): # wird größer desto quadratischer die seitenlängen x und y sind, x==y ist unendlich quadratisch
+    if (x == y):
+        return float("inf")
+    else:
+        return abs(1/(1-x/y))
 
 if __name__ == '__main__':
     if len(argv) < 2:
-        users = int(input("Anzahl der Interessenten: "))
-        y = int(input("Höhe des Grundstücks"))
-        x = int(input("Breite des Grundstücks"))
+        I = int(input("Anzahl der Interessenten: "))
+        Y = int(input("Höhe des Grundstücks: "))
+        X = int(input("Breite des Grundstücks: "))
     else:
         with open(argv[1]) as f:
-            users,Y,X = map(int, f.readlines())
+            I,Y,X = map(int, f.readlines())
 
-    print(users, Y, X)
+    if "-v" in argv:
+        print("Daten:", I, Y, X)
+    best = None
+    best_q = 0
+
+
+    for x in range(1, I+1):
+        y = ceil(I/x) #
+        while q(X/x,Y/y) < q(X/x,Y/(y+1)) and x*y < I*1.1:
+            y+=1
+
+        if best_q < q(X/x,Y/y):
+            best = (x,y)
+            best_q = q(X/x,Y/y)
+
+            if "-v" in argv:
+                print ("Neue beste Aufteilung gefunden: ", x, "*", y, "a", x*y, "Gärten")
+        # print(x,y,q(X/x,Y/y))
+
+    if "-v" in argv:
+        print("\nErgebnis:", best[0], "*", best[1], "a", best[0]*best[1], "Gärten")
+    else:
+        print(best[0])
+        print(best[1])
+        print(best[0]*best[0])
