b49b5dae9e942037ab8d07aff3f8670fa182fb71
Valentin Singer <SingerV-gar@gar-vs.de>
2024-11-16 16:50:15 +0100

    q(x,y) wird jetzt kleiner desto quadratischer sodass man ncihtmehr durch 0 teilt

--- aufgabe1/main.py
+++ aufgabe1/main.py
@@ -5,11 +5,8 @@ from sys import argv
 import math
 
 def q(x,y): # Funktion zur Bestimmung der "Quadratischheit" eines Rechtecks
-            # q(x,y) wird größer desto quadratischer die seitenlängen x und y sind, x==y ist "unendlich" quadratisch
-    if (x == y):
-        return math.inf
-    else:
-        return abs(1/(1-x/y))
+            # q(x,y) wird kleiner desto quadratischer die seitenlängen x und y sind, für x==y ist q(x,y) = 0
+    return abs(1-x/y)
 
 if __name__ == '__main__':
     if len(argv) < 2:
@@ -22,22 +19,24 @@ if __name__ == '__main__':
 
     if "-v" in argv:
         print("Daten:", I, Y, X)
+
     best = None
-    best_q = 0
+    best_q = math.inf # am anfang gibt es keine beste konfiguration
 
     for x in range(1, I+1):
-        y = math.ceil(I/x) # Wenn kein int rauskommt, Aufrunden sodass man knapp über I ist
+        y = math.ceil(I/x) # Wenn kein int rauskommt, Aufrunden sodass man knapp über x ist
         
-        while q(X/x,Y/y) < q(X/x,Y/(y+1)) and x*y < I*1.1: # solange die "Quadratischheit" größer wird und
+        while q(X/x,Y/(y+1)) < q(X/x,Y/y) and x*y < I*1.1: # solange q(x,y) kleiner wird,
+                                                           # das heißt das Rechteck quadratischer wird und
             y+=1                                           # man sich unter den 10% befindet, y erhöhen
 
-        if best_q < q(X/x,Y/y): # Falls ein neuer bester Wert gefunden wurde
+        # print(x,y,q(X/x,Y/y))
+        if q(X/x,Y/y) < best_q : # Falls ein neuer bester Wert gefunden wurde
             best = (x,y)
             best_q = q(X/x,Y/y)
 
             if "-v" in argv:
                 print ("Neue Bestaufteilung: ", x, "*", y, "≙", x*y, "Gärten", f"(Bewertung: {q(X/x, Y/y):.2f})")
-        # print(x,y,q(X/x,Y/y))
 
     if "-v" in argv:
         print("\nErgebnis:", best[0], "*", best[1], "≙", best[0]*best[1], "Gärten")
